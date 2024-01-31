904489818e23cf4b1e67a61718625c7f76416f9a
Anthony Metzidis <anthony.metzidis@gmail.com>
2024-01-30 17:41:47 -0800

    quiet

--- app/migrate-all.sh
+++ app/migrate-all.sh
@@ -9,7 +9,7 @@ done
 
 # save metadata
 echo "uploading metadata to nps_public_data.meta"
-bq load --autodetect --source_format NEWLINE_DELIMITED_JSON  nps_public_data.meta  "data/jsonl/meta.json"
+bq load -q --headless --autodetect --source_format NEWLINE_DELIMITED_JSON  nps_public_data.meta  "data/jsonl/meta.json"
 if [ $? -ne 0 ]; then
     echo "error: bq load. endpoint = $endpoint, table=$table"
     exit 1
